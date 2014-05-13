e939d5b048f6cbc601ac9c4d44c4a1f2fa4077ff
Matthieu Garrigues <matthieu.garrigues@gmail.com>
2014-05-13 10:40:17 +0000

    C++14 notes on readme and example.

--- example.cc
+++ example.cc
@@ -19,7 +19,9 @@ int main()
   auto person = iod(
     *name = "Philippe", // stared fields are serialized.
     *age = 42,
-    inc_age = [] (auto& self, int inc) { self.age += inc; },
+
+    inc_age = [] (auto& self, int inc) { self.age += inc; }, // Requires C++14.
+
     *cities = {"Paris", "Toronto", "New York City"},
     *cars = {
         iod(*name = "Renault", model = "Clio"), // All elements of an array must have the same type.

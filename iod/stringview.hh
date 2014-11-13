e11e88ba2304da7b1603167035b8609c240ccbd9
Matthieu Garrigues <matthieu.garrigues@gmail.com>
2014-11-13 17:05:56 +0100

    Stringview utility to mirror a string without extra copy.

---
+++ iod/stringview.hh
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <string>
+
+namespace iod
+{
+  
+  struct stringview
+  {
+    stringview() {}
+    stringview(const std::string& _str) : str(&_str[0]), len(_str.size()) {}
+    stringview(const char* _str, int _len) : str(_str), len(_len) {}
+    stringview(const char* _str) : str(_str), len(strlen(_str)) {}
+      
+    bool operator==(const stringview& o) const { return len == o.len and !strncmp(o.str, str, len); }
+    bool operator==(const std::string& o) const { return len == o.size() and !strncmp(&o[0], str, len); }
+    bool operator==(const char* o) const { return len == strlen(o) and !strncmp(o, str, len); }
+
+    bool operator<(const stringview& o) const { return strncmp(o.str, str, std::min(len, o.len)); }
+    explicit operator std::string() const { return std::string(str, len); }
+
+    auto& operator[](int p) { return str[p]; }
+    const auto& operator[](int p) const { return str[p]; }
+
+    int size() const { return len; }
+    const char* data() const { return str; }
+    auto to_std_string() const { return std::string(str, len); }
+
+    const char* str;
+    int len;
+  };
+
+}

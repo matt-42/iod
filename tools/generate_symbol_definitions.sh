72804da68e667b513d39be0fa5e8f246f00be2b0
Matthieu Garrigues <matthieu.garrigues@gmail.com>
2014-11-15 15:41:07 +0100

    Fix symbol header generation script.

--- tools/generate_symbol_definitions.sh
+++ tools/generate_symbol_definitions.sh
@@ -1,1 +1,1 @@
-sed -e 's/^\([a-zA-Z1-9_]\)\([a-zA-Z1-9_]*\)/#ifndef IOD_SYMBOL__\U\1\L\2\E\n\#define IOD_SYMBOL__\U\1\L\2\E\n    iod_define_symbol(\1\2, _\U\1\L\2\E)\n#endif/'
+sed -e 's/^\([a-zA-Z1-9_]\)\([a-zA-Z1-9_]*\)/#ifndef IOD_SYMBOL__\U\1\L\2\E\n\#define IOD_SYMBOL__\U\1\L\2\E\n    iod_define_symbol(\1\2, _\U\1\L\2\E)\n#endif/' $1

da7a1b95255489675facf33c045ab8ec80603d93
Matthieu Garrigues <matthieu.garrigues@gmail.com>
2015-01-17 11:32:15 +0100

    Test bind method.

---
+++ tests/bind_method.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cassert>
+#include <iod/bind_method.hh>
+
+struct A
+{
+  int add(int a, float b) { return a + b; }
+};
+
+int main()
+{
+  using namespace iod;
+
+  A a;
+  auto f = bind_method(a, &A::add);
+  assert(f(1,2) == 3);
+}
